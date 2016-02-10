66dc576a7a9da2433919777246403224d3c4ea12
Théo Rossier <theorossier@gmail.com>
2016-02-10 21:27:37 +0100

    ok

--- jobs/google_calendar.rb
+++ jobs/google_calendar.rb
@@ -59,62 +59,62 @@
 
 
 
-require 'google/api_client'
-require 'google/api_client/client_secrets'
-require 'google/api_client/auth/installed_app'
-require 'google/api_client/auth/storage'
-require 'google/api_client/auth/storages/file_store'
-require 'fileutils'
+# require 'google/api_client'
+# require 'google/api_client/client_secrets'
+# require 'google/api_client/auth/installed_app'
+# require 'google/api_client/auth/storage'
+# require 'google/api_client/auth/storages/file_store'
+# require 'fileutils'
 
-APPLICATION_NAME = 'dashing.io.google.calendar'
-CLIENT_SECRETS_PATH = 'jobs/client_secret.json'
-CREDENTIALS_PATH = File.join(Dir.home, '.credentials',
-                             "google-calendar-dashing-job.json")
-SCOPE = 'https://www.googleapis.com/auth/calendar.readonly'
+# APPLICATION_NAME = 'dashing.io.google.calendar'
+# CLIENT_SECRETS_PATH = 'jobs/client_secret.json'
+# CREDENTIALS_PATH = File.join(Dir.home, '.credentials',
+#                              "google-calendar-dashing-job.json")
+# SCOPE = 'https://www.googleapis.com/auth/calendar.readonly'
 
-##
-# Ensure valid credentials, either by restoring from the saved credentials
-# files or intitiating an OAuth2 authorization request via InstalledAppFlow.
-# If authorization is required, the user's default browser will be launched
-# to approve the request.
-#
-# @return [Signet::OAuth2::Client] OAuth2 credentials
-def authorize
-  FileUtils.mkdir_p(File.dirname(CREDENTIALS_PATH))
+# ##
+# # Ensure valid credentials, either by restoring from the saved credentials
+# # files or intitiating an OAuth2 authorization request via InstalledAppFlow.
+# # If authorization is required, the user's default browser will be launched
+# # to approve the request.
+# #
+# # @return [Signet::OAuth2::Client] OAuth2 credentials
+# def authorize
+#   FileUtils.mkdir_p(File.dirname(CREDENTIALS_PATH))
 
-  file_store = Google::APIClient::FileStore.new(CREDENTIALS_PATH)
-  storage = Google::APIClient::Storage.new(file_store)
-  auth = storage.authorize
+#   file_store = Google::APIClient::FileStore.new(CREDENTIALS_PATH)
+#   storage = Google::APIClient::Storage.new(file_store)
+#   auth = storage.authorize
 
-  if auth.nil? || (auth.expired? && auth.refresh_token.nil?)
-    app_info = Google::APIClient::ClientSecrets.load(CLIENT_SECRETS_PATH)
-    flow = Google::APIClient::InstalledAppFlow.new({
-      :client_id => app_info.client_id,
-      :client_secret => app_info.client_secret,
-      :scope => SCOPE})
-    auth = flow.authorize(storage)
-    puts "Credentials saved to #{CREDENTIALS_PATH}" unless auth.nil?
-  end
-  auth
-end
+#   if auth.nil? || (auth.expired? && auth.refresh_token.nil?)
+#     app_info = Google::APIClient::ClientSecrets.load(CLIENT_SECRETS_PATH)
+#     flow = Google::APIClient::InstalledAppFlow.new({
+#       :client_id => app_info.client_id,
+#       :client_secret => app_info.client_secret,
+#       :scope => SCOPE})
+#     auth = flow.authorize(storage)
+#     puts "Credentials saved to #{CREDENTIALS_PATH}" unless auth.nil?
+#   end
+#   auth
+# end
 
-# Initialize the API
-client = Google::APIClient.new(:application_name => APPLICATION_NAME)
-client.authorization = authorize
-calendar_api = client.discovered_api('calendar', 'v3')
+# # Initialize the API
+# client = Google::APIClient.new(:application_name => APPLICATION_NAME)
+# client.authorization = authorize
+# calendar_api = client.discovered_api('calendar', 'v3')
 
-# Start the scheduler
-SCHEDULER.every '15m', :first_in => 4 do |job|
+# # Start the scheduler
+# SCHEDULER.every '15m', :first_in => 4 do |job|
 
-  results = client.execute!(
-    :api_method => calendar_api.events.list,
-    :parameters => {
-      :calendarId => 'primary',
-      :maxResults => 3,
-      :singleEvents => true,
-      :orderBy => 'startTime',
-      :timeMin => Time.now.iso8601 })
+#   results = client.execute!(
+#     :api_method => calendar_api.events.list,
+#     :parameters => {
+#       :calendarId => 'primary',
+#       :maxResults => 3,
+#       :singleEvents => true,
+#       :orderBy => 'startTime',
+#       :timeMin => Time.now.iso8601 })
 
-  send_event('google_calendar', { events: results.data })
+#   send_event('google_calendar', { events: results.data })
 
-end
+# end
